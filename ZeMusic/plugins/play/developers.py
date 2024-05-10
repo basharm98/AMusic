bbdfc8b113001155fd3218772f1e62da80093ec7
basharm98 <167342964+basharm98@users.noreply.github.com>
2024-05-11 00:38:55 +0200

    Update developers.py

--- ZeMusic/plugins/play/developers.py
+++ ZeMusic/plugins/play/developers.py
@@ -19,7 +19,7 @@ from random import  choice, randint
   command(["المبرمج","مبرمج السورس","مبرمج","مطور السورس"])
 )
 async def huhh(client: Client, message: Message):
-    dev_id = 5145609515
+    dev_id = 6992029895
     dev = await client.get_users(dev_id)
     name = dev.first_name
     usrnam = dev.username
@@ -36,7 +36,7 @@ async def huhh(client: Client, message: Message):
                          name, url=f"https://t.me/{usrnam}"), 
                  ],[
                    InlineKeyboardButton(
-                        "•✯ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙺𝙸𝙽𝙶 ✯•", url=f"https://t.me/EF_19"),
+                        "•✯ 𝚂𝙾𝚄𝚁𝙲𝙴 Z3EIM ✯•", url=f"https://t.me/Z3EIM"),
                 ],
 
             ]
